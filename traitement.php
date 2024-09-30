8cf67fee6d7c35bce9e8c75e58faa8c56ed590d8
Ike David Baali <ikebaalidavid@live.de>
2024-09-30 22:26:37 +0200

    final_projet_30_Sep_2024

--- traitement.php
+++ traitement.php
@@ -1,13 +1,29 @@
 <?php
 // Code à exécuter si l'un des champs est vide
-if (empty($_POST['titre']) || empty($_POST['artiste']) || empty($_POST['image']) || empty($_POST['description']))
-|| !filter_var($_POST['image'], FILTER_VALIDE_URL) 
-|| strlen($_POST['description']) < 3 {
-    
+if (empty($_POST['titre']) 
+    || empty($_POST['artiste']) 
+    || empty($_POST['image']) 
+    || empty($_POST['description'])
+    || !filter_var($_POST['image'], FILTER_VALIDATE_URL) 
+    || strlen($_POST['description']) < 3 ) {
     // redirection sur le formulaire
-    header('location = ajouter.php');
+    header('location: ajouter.php?erreur=true');
 } else {
-    // inser
+    $titre = htmlspecialchars($_POST['titre']);
+    $artiste = htmlspecialchars($_POST['artiste']);
+    $image = htmlspecialchars($_POST['image']);
+    $description = htmlspecialchars($_POST['description']);
+
+    include 'bdd.php';
+    $bdd = connexion();
+
+    $requete = $bdd->prepare('INSERT INTO oeuvres (titre, artiste, image, description) VALUES (?, ?, ?, ?)');
+    $requete->execute([$titre, $artiste, $image, $description]);
+    
+    // redirection sur le formulaire derniere id ajouté
+    header('Location: oeuvre.php?id=' . $bdd->lastInsertId());
+
 }
+
 
 ?>
